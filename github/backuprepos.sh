0f56690288ab4ef4b77b21fc9516552230b8a37f
Huluvu424242 <huluvu424242@gmail.com>
2020-02-14 17:25:45 +0100

    github backup added

---
+++ github/backuprepos.sh
@@ -0,0 +1,205 @@
+#!/bin/sh
+
+
+# [0] clone 1jar-maven-plugin
+git clone --mirror https://github.com/FunThomas424242/1jar-maven-plugin.git
+cd 1jar-maven-plugin.git;git remote update; cd ..
+
+# [1] clone ahnen.dsl
+git clone --mirror https://github.com/FunThomas424242/ahnen.dsl.git
+cd ahnen.dsl.git;git remote update; cd ..
+
+# [2] clone algorithmen.example
+git clone --mirror https://github.com/FunThomas424242/algorithmen.example.git
+cd algorithmen.example.git;git remote update; cd ..
+
+# [3] clone angular-webcomponents.template
+git clone --mirror https://github.com/FunThomas424242/angular-webcomponents.template.git
+cd angular-webcomponents.template.git;git remote update; cd ..
+
+# [4] clone annotation-processor-plugin.example
+git clone --mirror https://github.com/FunThomas424242/annotation-processor-plugin.example.git
+cd annotation-processor-plugin.example.git;git remote update; cd ..
+
+# [5] clone annotation-processor-xtend.example
+git clone --mirror https://github.com/FunThomas424242/annotation-processor-xtend.example.git
+cd annotation-processor-xtend.example.git;git remote update; cd ..
+
+# [6] clone ant-ube-task
+git clone --mirror https://github.com/FunThomas424242/ant-ube-task.git
+cd ant-ube-task.git;git remote update; cd ..
+
+# [7] clone anttask-githubmirror-userrepos
+git clone --mirror https://github.com/FunThomas424242/anttask-githubmirror-userrepos.git
+cd anttask-githubmirror-userrepos.git;git remote update; cd ..
+
+# [8] clone awk
+git clone --mirror https://github.com/FunThomas424242/awk.git
+cd awk.git;git remote update; cd ..
+
+# [9] clone base64-downloader
+git clone --mirror https://github.com/FunThomas424242/base64-downloader.git
+cd base64-downloader.git;git remote update; cd ..
+
+# [10] clone blender.example
+git clone --mirror https://github.com/FunThomas424242/blender.example.git
+cd blender.example.git;git remote update; cd ..
+
+# [11] clone bndtools.example
+git clone --mirror https://github.com/FunThomas424242/bndtools.example.git
+cd bndtools.example.git;git remote update; cd ..
+
+# [12] clone books.example
+git clone --mirror https://github.com/FunThomas424242/books.example.git
+cd books.example.git;git remote update; cd ..
+
+# [13] clone BoostX
+git clone --mirror https://github.com/FunThomas424242/BoostX.git
+cd BoostX.git;git remote update; cd ..
+
+# [14] clone BuildBoost
+git clone --mirror https://github.com/FunThomas424242/BuildBoost.git
+cd BuildBoost.git;git remote update; cd ..
+
+# [15] clone BuildBoost.dev
+git clone --mirror https://github.com/FunThomas424242/BuildBoost.dev.git
+cd BuildBoost.dev.git;git remote update; cd ..
+
+# [16] clone buildboost.example
+git clone --mirror https://github.com/FunThomas424242/buildboost.example.git
+cd buildboost.example.git;git remote update; cd ..
+
+# [17] clone buildr.example
+git clone --mirror https://github.com/FunThomas424242/buildr.example.git
+cd buildr.example.git;git remote update; cd ..
+
+# [18] clone bundles
+git clone --mirror https://github.com/FunThomas424242/bundles.git
+cd bundles.git;git remote update; cd ..
+
+# [19] clone cnf
+git clone --mirror https://github.com/FunThomas424242/cnf.git
+cd cnf.git;git remote update; cd ..
+
+# [20] clone codersdojo.katas
+git clone --mirror https://github.com/FunThomas424242/codersdojo.katas.git
+cd codersdojo.katas.git;git remote update; cd ..
+
+# [21] clone com.github.funthomas424242.rades.evm
+git clone --mirror https://github.com/FunThomas424242/com.github.funthomas424242.rades.evm.git
+cd com.github.funthomas424242.rades.evm.git;git remote update; cd ..
+
+# [22] clone com.github.funthomas424242.rades.evm.generator
+git clone --mirror https://github.com/FunThomas424242/com.github.funthomas424242.rades.evm.generator.git
+cd com.github.funthomas424242.rades.evm.generator.git;git remote update; cd ..
+
+# [23] clone CryptMan
+git clone --mirror https://github.com/FunThomas424242/CryptMan.git
+cd CryptMan.git;git remote update; cd ..
+
+# [24] clone cucumber-js-selenium-webdriver-example
+git clone --mirror https://github.com/FunThomas424242/cucumber-js-selenium-webdriver-example.git
+cd cucumber-js-selenium-webdriver-example.git;git remote update; cd ..
+
+# [25] clone cucumber.example
+git clone --mirror https://github.com/FunThomas424242/cucumber.example.git
+cd cucumber.example.git;git remote update; cd ..
+
+# [26] clone desktop-url-navigator
+git clone --mirror https://github.com/FunThomas424242/desktop-url-navigator.git
+cd desktop-url-navigator.git;git remote update; cd ..
+
+# [27] clone development-environment.eclipse-project
+git clone --mirror https://github.com/FunThomas424242/development-environment.eclipse-project.git
+cd development-environment.eclipse-project.git;git remote update; cd ..
+
+# [28] clone devtools
+git clone --mirror https://github.com/FunThomas424242/devtools.git
+cd devtools.git;git remote update; cd ..
+
+# [29] clone devtools-preferences.eclipse-plugin
+git clone --mirror https://github.com/FunThomas424242/devtools-preferences.eclipse-plugin.git
+cd devtools-preferences.eclipse-plugin.git;git remote update; cd ..
+
+# [30] clone nitrite-spring-boot-starter
+git clone --mirror https://github.com/FunThomas424242/nitrite-spring-boot-starter.git
+cd nitrite-spring-boot-starter.git;git remote update; cd ..
+
+# [31] clone jenkinsmonitor
+git clone --mirror https://github.com/FunThomas424242/jenkinsmonitor.git
+cd jenkinsmonitor.git;git remote update; cd ..
+
+# [32] clone jar-install-plugin
+git clone --mirror https://github.com/FunThomas424242/jar-install-plugin.git
+cd jar-install-plugin.git;git remote update; cd ..
+
+# [33] clone mypocketmod
+git clone --mirror https://github.com/FunThomas424242/mypocketmod.git
+cd mypocketmod.git;git remote update; cd ..
+
+# [34] clone free-images
+git clone --mirror https://github.com/FunThomas424242/free-images.git
+cd free-images.git;git remote update; cd ..
+
+# [35] clone rezeptbuch
+git clone --mirror https://github.com/FunThomas424242/rezeptbuch.git
+cd rezeptbuch.git;git remote update; cd ..
+
+# [36] clone org.gui4j
+git clone --mirror https://github.com/FunThomas424242/org.gui4j.git
+cd org.gui4j.git;git remote update; cd ..
+
+# [37] clone rezeptsamnmlung
+git clone --mirror https://github.com/FunThomas424242/rezeptsammlung.git
+cd rezeptsammlung.git;git remote update; cd ..
+
+# [38] clone webspeech.example
+git clone --mirror https://github.com/FunThomas424242/webspeech.example.git
+cd webspeech.example.git;git remote update; cd ..
+
+# [39] clone junit.example
+git clone --mirror https://github.com/FunThomas424242/junit.example.git
+cd junit.example.git;git remote update; cd ..
+
+# [40] clone plantuml-maven-plugin
+git clone --mirror https://github.com/FunThomas424242/plantuml-maven-plugin.git
+cd plantuml-maven-plugin.git;git remote update; cd ..
+
+# [41] clone honey-testcomponent
+git clone --mirror https://github.com/FunThomas424242/honey-testcomponent.git
+cd honey-testcomponent.git;git remote update; cd ..
+
+# [42] clone liona-speech-input
+git clone --mirror https://github.com/FunThomas424242/liona-speech-input.git
+cd liona-speech-input.git;git remote update; cd ..
+
+# [43] clone liona-slideshow
+git clone --mirror https://github.com/FunThomas424242/liona-slideshow.git
+cd liona-slideshow.git;git remote update; cd ..
+
+# [44] clone liona-toggle-button
+git clone --mirror https://github.com/FunThomas424242/liona-toggle-button.git
+cd liona-toggle-button.git;git remote update; cd ..
+
+# [45] clone diplomarbeit
+git clone --mirror https://github.com/FunThomas424242/diplomarbeit.git
+cd diplomarbeit.git;git remote update; cd ..
+
+# [46] clone home
+git clone --mirror https://github.com/FunThomas424242/home.git
+cd home.git;git remote update; cd ..
+
+# [47] clone job-dealer
+git clone --mirror https://github.com/FunThomas424242/job-dealer.git
+cd job-dealer.git;git remote update; cd ..
+
+# [48] clone jsdoc3-maven-plugin
+git clone --mirror https://github.com/FunThomas424242/jsdoc3-maven-plugin.git
+cd jsdoc3-maven-plugin.git;git remote update; cd ..
+
+# [49] clone rezepte
+git clone --mirror https://github.com/FunThomas424242/rezepte.git
+cd rezepte.git;git remote update; cd ..
+
+
+
