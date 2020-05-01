22249cd4b67f1c275c37151077026adc25f7ff81
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-05-01 14:21:00 +0200

    doku neu generiert
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

---
+++ docs/1.0.0-SNAPSHOT/hobbies/eistanz/Eislaufen.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1148328205671" ID="Freemind_Link_1252028690" MODIFIED="1167916363828" STYLE="bubble" TEXT="Eislaufen">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1167844616078" ID="_" MODIFIED="1167916372203" POSITION="right" TEXT="Eistanz">
+<node CREATED="1167916398390" ID="Freemind_Link_1627009440" MODIFIED="1167916402093" TEXT="Spurenbilder"/>
+</node>
+<node CREATED="1167916386937" ID="Freemind_Link_1141688310" MODIFIED="1167916389984" POSITION="left" TEXT="Formation"/>
+<node CREATED="1167844633546" ID="Freemind_Link_603108216" MODIFIED="1167916381625" POSITION="left" TEXT="Eiskunstlauf"/>
+<node CREATED="1167916408843" ID="Freemind_Link_211613326" MODIFIED="1167916456968" POSITION="right" TEXT="Grundlagen">
+<node CREATED="1167916459593" ID="Freemind_Link_723809141" MODIFIED="1167916466015" TEXT="Begriffe und Definitionen"/>
+<node CREATED="1167916425031" ID="Freemind_Link_1678945358" MODIFIED="1167916445328" TEXT="Laufschritt (&#xfc;bersetzen)">
+<node CREATED="1167916562640" ID="Freemind_Link_1751022895" MODIFIED="1167916856500" TEXT="Notizen&#xa;&#xa;&#xdc;bersetzen links vorw&#xe4;rts&#xa;+ Linken Fuss links neben rechten Fuss ransetzen an H&#xe4;lfte nicht davor, dann erst neu mit rechts Schwung holen und den rechten Fuss vorn &#xfc;bersetzen (beim Eistanz exakt in Linie vor dem linken, sonst weit nach links)&#xa;+ stets im Knie bleiben&#xa;+ Blick immer auf H&#xf6;he Bande ausrichten (frei nach vorn)&#xa;+ nicht den Oberk&#xf6;rper zu weit nach vorn (wir verbeugen uns nicht)&#xa;&#xa;Anwendung im Eistanz&#xa;+ Walz Movement&#xa;"/>
+</node>
+</node>
+</node>
+</map>
